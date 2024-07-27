4c189153a77806862043cd6d9e76ea21228a34f4
jonahkayizzi <jonahkayizzi@gmail.com>
2024-07-27 14:18:19 +0200

    Implement project structure, add depencies and testing

---
+++ src/main/java/com/prognosis/cli/App.java
@@ -0,0 +1,10 @@
+package com.prognosis.cli;
+
+import com.prognosis.cli.model.User;
+import com.prognosis.cli.model.Admin;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
