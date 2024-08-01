8110bdba3757aa2b9cfec3b1ab9760750dc1ca84
jonahkayizzi <jonahkayizzi@gmail.com>
2024-08-01 15:48:26 +0200

    Merge updated main to current branch

--- scripts/register_patient.sh
+++ scripts/register_patient.sh
@@ -10,30 +10,33 @@ dob=$4
 hiv_status=$5
 diagnosis_date=$6
 on_art=$7
-country=$8
-user_id=$9
+art_start_date=$8
+country=$9
+user_id=$10
 
 #hash password for storing
 stored_hashed_password=$(openssl passwd -1 -salt "$salt" "$password")
 # store new user credentials in the data store
 
 #file path to store user data
-user_store="../user_store.txt"
+
+user_store="$PWD/user-store.txt"
 
 #update like with given user id
-awk -v user_id="$user_id" -v first_name="$first_name" -v last_name="$last_name" -v dob="$dob" -v hiv_status="$hiv_status" -v country="$country" -v stored_hashed_password="$stored_hashed_password" -v user_type="patient" -v email="$email" -v diagnosis_date="$diagnosis_date" -v on_art="$on_art" '
+awk -v user_id="$user_id" -v first_name="$first_name" -v last_name="$last_name" -v dob="$dob" -v hiv_status="$hiv_status" -v country="$country" -v stored_hashed_password="$stored_hashed_password" -v user_type="patient" -v email="$email" -v diagnosis_date="$diagnosis_date" -v on_art="$on_art" -v art_start_date="$art_start_date" '
 BEGIN { OFS=" " } 
 {
     if ($1 == user_id) {
-        $2 = stored_hashed_password
-        $3 = user_type
-        $4 = first_name
-        $5 = last_name 
-        $6 = dob
-        $7 = hiv_status
-        $8 = country
-        $9 = diagnosis_date
-        $10 = on_art
+        $5 = stored_hashed_password
+        $6 = user_type
+        $7 = first_name
+        $8 = last_name 
+        $9 = dob
+        $10 = hiv_status
+        $11 = country
+        $12 = diagnosis_date
+        $13 = on_art
+        $14 = art_start_date
     } 
     print 
 }' "$user_store" > temp && mv temp "$user_store"
