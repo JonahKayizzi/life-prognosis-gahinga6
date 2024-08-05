566946e93c0bb2ce1599bbc1ac8b3b432ae6d79a
jonahkayizzi <jonahkayizzi@gmail.com>
2024-08-05 23:27:43 +0200

    Move UI methods to the controller

--- src/main/java/com/prognosis/cli/view/PatientView.java
+++ src/main/java/com/prognosis/cli/view/PatientView.java
@@ -26,51 +26,6 @@ public class PatientView implements UserView {
         }
     }
 
-    public String promptFirstName() {
-        System.out.println("Enter your first name:");
-        return System.console().readLine();
-    }
-
-    public String promptLastName() {
-        System.out.println("Enter your last name:");
-        return System.console().readLine();
-    }
-
-    public String promptDateOfBirth() {
-        System.out.println("Enter your date of birth:");
-        return System.console().readLine();
-    }
-
-    public String promptHivStatus() {
-        System.out.println("Enter your HIV status:");
-        return System.console().readLine();
-    }
-
-    public String promptDateOfDiagnosis() {
-        System.out.println("Enter your date of diagnosis:");
-        return System.console().readLine();
-    }
-
-
-    public String promptIsOnART() {
-        System.out.println("Are you taking anti retro viral drugs:");
-        return System.console().readLine();
-    }
-
-    public String promptArtStartDate() {
-        System.out.println("Enter the date you started taking  anti retro viral drugs:");
-        return System.console().readLine();
-    }
-
-    public String promptCountry() {
-        System.out.println("Enter your country of residence:");
-        return System.console().readLine();
-    }
-
-    public void displayCode(String code) {
-        System.out.println(String.format("Use this code to register: %s", code));
-    }
-
     @Override
     public void displayErrorMessage(String message) {
         System.out.println(message);
