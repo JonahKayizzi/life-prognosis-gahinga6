ca5d12958ffc2bb797035c613162ad8afacd15ce
jonahkayizzi <jonahkayizzi@gmail.com>
2024-08-07 14:38:11 +0200

    Add a login script to stored logged in user to session storage

--- scripts/user_login.sh
+++ scripts/user_login.sh
@@ -4,12 +4,13 @@
 email=$1
 password=$2
 #hash password for before checking user records
-hashed_password=$(openssl passwd -1 -salt "$salt" "$password")
+hashed_password=$(openssl passwd -1 -salt "salt" "$password")
 escaped_password=$(echo "$hashed_password" | sed 's/\$/\\$/g')
 # return the record matching the supplied email and password
 user_record=$(grep "$email.*$escaped_password" "$PWD/user-store.txt")
 #check if record exits
 if [ -n "$user_record" ]; then
+    echo $user_record > "$PWD/session-storage.txt"
     #check for the user type
     user_id=$(echo "$user_record" | awk '{print $1}')
     user_type=$(echo "$user_record" | awk '{print $2}')
